dc7af8f0e1a7add681359bf0ea5b668f6684743c
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-05-23 23:50:00 -0500

    added pomodoro gif

--- src/components/ProjectsPage.jsx
+++ src/components/ProjectsPage.jsx
@@ -7,6 +7,8 @@ import quoteGenerator from '../assets/quoteGenerator.gif';
 import quoteGeneratorStatic from '../assets/quoteGeneratorStatic.jpg';
 import fitness from '../assets/fitness.gif';
 import fitnessStatic from '../assets/fitnessStatic.jpg';
+import timer from '../assets/timer.gif';
+import timerStatic from '../assets/timerStatic.jpg';
 
 const Projects = () => {
   
@@ -22,11 +24,11 @@ const Projects = () => {
     <div className="GenericContainer">
       <h1>Projects</h1>
       <div className="ProjectsGrid">
-        <Project title="PORTFOLIO" description="a portfolio website within a portfolio website. #recursion" gif={[calculator,calculatorStatic]}/>
-        <Project title="FITNESS TRACKING" description='A small, responsive full-stack app to keep track of your workouts.' gif={[fitness,fitnessStatic]}/>
-        <Project title="RANDOM QUOTE GENERATOR" description='An iOS-inspired random quote generator.' gif={[quoteGenerator, quoteGeneratorStatic]}/>
-        <Project title="HELLO WORLD" gif={[quoteGenerator, quoteGenerator]}/>
-        <Project title="HELLO WORLD" gif={[quoteGenerator, quoteGenerator]}/>
+        <Project title="CALCULATOR" description="a nice looking, fully functioning calculator built on react." gif={[calculator,calculatorStatic]}/>
+        <Project title="FITNESS TRACKING" description='a small, responsive full-stack app to keep track of your workouts.' gif={[fitness,fitnessStatic]}/>
+        <Project title="RANDOM QUOTE GENERATOR" description='an iOS-inspired random quote generator.' gif={[quoteGenerator, quoteGeneratorStatic]}/>
+        <Project title="HELLO WORLD" description='a simple time management tool built on React.' gif={[timer, timerStatic]}/>
+        <Project title="POMODORO TIMER" gif={[quoteGenerator, quoteGenerator]}/>
         <Project title="HELLO WORLD" gif={[quoteGenerator, quoteGenerator]}/>
       </div>
       <div className="ActionButtonCluster">
