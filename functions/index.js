f091ee2a2d4f80ffa8b0a4936f9de98399f22c3e
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-06-06 16:32:44 -0500

    removed and changed api key

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require("firebase-functions");
 const axios = require('axios');
+require('dotenv').config();
 
 exports.testTogglCall = functions.https.onRequest( async (request, response) => {
     try {
@@ -8,7 +9,7 @@ exports.testTogglCall = functions.https.onRequest( async (request, response) => 
             url: 'https://api.track.toggl.com/api/v8/me',
             accept: 'application/json',
             auth: {
-                username: '07677d9a20a59edd57848c9d0574fdf0',
+                username: process.env.TOGGL_API_KEY,
                 password: 'api_token'
             }
         })
