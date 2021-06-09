7ea5e4c6fdadf43187cca3ef89b2b0da03dc686d
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-06-08 20:57:01 -0500

    reports call working

--- functions/index.js
+++ functions/index.js
@@ -36,25 +36,42 @@ exports.getWorkspaceTags = functions.https.onRequest(async (request, response) =
 
 exports.getReportsData = functions.https.onRequest(async (request, response) => {
     try {
-        //plan:
-        //do first initial call to get total number of records
-        //for each n = total / 50, make a call for page n and append to total data
-        //define a projectTimes object with key:value === project:timeWorkedOn for all X projects to be displayed on website
         //then, for each key in all records object, match the 'descriptions' to the projects (pass the project descriptions as an args array, 
         //      e.g. ['Building Portfolio Website']) and add the 'dur' to the value of projectTimes' key
-        //finally, send projectTimes as response  
-        const result = await axiosClient.get(`/reports/api/v2/details`, {
-            params: {
-                'user_agent': process.env.USER_AGENT,
-                'workspace_id': process.env.WORKSPACE_ID,
-                'tag_ids': 9306704,
-                'since': '2021-02-01',
-                'page': 1
-            }})
-        response.send(result.data);
-        // console.log(Object.keys(result.data.data).length);
-        // console.log(result.data);
-        return result
+        //finally, send projectTimes as response     
+        
+        const getPage = async (pageNum = 1) => {
+            const reportData = await axiosClient.get(`/reports/api/v2/details`, {
+                params: {
+                    'user_agent': process.env.USER_AGENT,
+                    'workspace_id': process.env.WORKSPACE_ID,
+                    'tag_ids': 9306704,
+                    'since': '2021-02-01',
+                    'page': pageNum
+                }})
+                
+                return reportData.data
+            }
+        
+        const firstPage = await getPage(1);
+        let totalPages = Math.ceil(firstPage.total_count/50);  
+        let totalData = []
+        
+        for (let i = 1; i <= totalPages; i++) {
+            const page = getPage(i);
+            totalData.push(page);
+        }
+
+        let finalData = await Promise.all(totalData);
+        finalData = finalData.flat();
+        
+        console.log(`totalPages = ${totalData}`)
+        console.log(`totalPages = ${totalPages}`)
+        console.log(`totalData length = ${totalData.length}`)
+        console.log(finalData.length);
+        console.log(finalData[0].data[49]);
+
+        response.send(finalData);
     } catch (err) {
         console.error(err);
     }
