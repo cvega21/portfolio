f61060356d624a6f40b96aa4dda40b1f6457a87a
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-06-18 00:39:40 -0500

    added set project data in DB cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,9 @@
 const functions = require("firebase-functions");
+const admin = require('firebase-admin');
 const axios = require('axios').default;
 require('dotenv').config();
+
+admin.initializeApp()
 
 const axiosClient = axios.create({
     baseURL: 'https://api.track.toggl.com',
@@ -34,27 +37,40 @@ exports.getWorkspaceTags = functions.https.onRequest(async (request, response) =
 
 // #100DaysOfCode tag_id = 9306704
 
-// Next steps: make one data loader function (e.g. load in bulk from 1 year back) that loads into DB then cloud scheduler function will only pick up records after a certain point and add them to whatever's already there
+exports.getProjectsData = functions.https.onRequest(async (request, response) => {    
+    
+    const getPage = async (pageNum = 1) => {
+        // as Toggl's API has a per-call limit of 50 records, we have to loop through this function for however many total records we have
+        const reportData = await axiosClient.get(`/reports/api/v2/details`, {
+            params: {
+                'user_agent': process.env.USER_AGENT,
+                'workspace_id': process.env.WORKSPACE_ID,
+                'tag_ids': 9306704,
+                'since': '2021-01-01',
+                'page': pageNum
+            }})
+        return reportData.data
+    }
+    
+    const setProjectData = (project) => {
+        // sets project hours in firebase DB after they've been called from Toggl API and sanitized by cloud function
+        const keyToUpdate = admin.database().ref(`/projects/${project}`);
+        return keyToUpdate.set(projectsTime[project])
+    }
 
-exports.getProjectsData = functions.https.onRequest(async (request, response) => {
-    try {
-        const getPage = async (pageNum = 1) => {
-            const reportData = await axiosClient.get(`/reports/api/v2/details`, {
-                params: {
-                    'user_agent': process.env.USER_AGENT,
-                    'workspace_id': process.env.WORKSPACE_ID,
-                    'tag_ids': 9306704,
-                    'since': '2021-02-01',
-                    'page': pageNum
-                }})
-                
-                return reportData.data
-            }
-        
-        const firstPage = await getPage(1);
-        let totalPages = Math.ceil(firstPage.total_count/50);  
-        let totalData = []
-        
+    let projectsTime = {
+        "Build Portfolio Website": 0,
+        "Fitness Tracker App": 0,
+        "EDM Machine": 0,
+        "Calculator Project": 0,
+        "Pomodoro Timer Project": 0
+    }
+
+    let totalData = [];
+    
+    try {        
+        const totalPages = Math.ceil((await getPage(1)).total_count/50);  
+          
         for (let i = 1; i <= totalPages; i++) {
             const page = await getPage(i);
             totalData.push(page.data);
@@ -63,32 +79,20 @@ exports.getProjectsData = functions.https.onRequest(async (request, response) =>
         let finalData = await Promise.all(totalData);
         finalData = finalData.flat();
 
-        let projectsTime = {
-            "Build Portfolio Website": 0,
-            "Fitness Tracker App": 0,
-            "EDM Machine": 0,
-            "Calculator Project": 0,
-            "Pomodoro Timer Project": 0
-        }
-        
         finalData.forEach((record) => {
             if (projectsTime.hasOwnProperty(record.description)) {
-                if (record.description === 'Calculator Project') {
-                    console.log(`calculator project. duration in ms = ${record.dur}. duration in calculated hours (?) = ${record.dur/3600000}`)
-                }
-                // Adds hours tracked to 2 decimal points
+                // calculates hours to 2 decimal points and adds them to the object's key
                 projectsTime[record.description] = parseFloat(projectsTime[record.description]) + parseFloat((record.dur/3600000))
                 projectsTime[record.description] = projectsTime[record.description].toFixed(1);
             } else {
-                // console.log(`${record['description']} is not being tracked`)
-                // console.log(projectsTime[record])
+                // it's not a project in the projectsTime object
             }
         })
 
-        console.log(`totalPages = ${totalPages}`)
-        console.log(`totalData length = ${totalData.length}`)
-        console.log(finalData.length);
-        console.log(projectsTime);
+        for (let project in projectsTime) {
+            setProjectData(project);
+        }
+
         response.set('Access-Control-Allow-Origin', '*');
         response.send(projectsTime);
     } catch (err) {
