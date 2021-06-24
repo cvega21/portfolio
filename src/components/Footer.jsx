b48668675ed29530879a76461a8356982eaf4ce1
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-06-23 23:50:39 -0500

    added basic contact page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            <p>Made with <span>&#9829;</span> from Austin, TX</p>
+            <p>Made with <span>&#9829;</span> in Austin, TX</p>
             <p>© 2021 Christian Vega. All rights reserved.</p>
         </footer>
     )
--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -5,13 +5,13 @@ const Contact = (props) => {
 
   return (
     <div className="GenericContainer">
-      <div>
-        <ActionButton link="home" navigation="left" onChangeNav={props.onChangeNav}/>
-      <div className="ActionButtonCluster">
-      </div>
-        <h1>Get in touch!</h1>
-      <input></input>
-      </div>
+      <div className="ContactContainer">
+          <h1>Get in touch!</h1>
+          <input className="MessageContainer"></input>
+        </div>
+        <div className="ActionButtonCluster">
+          <ActionButton link="send" onChangeNav={props.onChangeNav}/>
+        </div>
     </div>
   )
 }
