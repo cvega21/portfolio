2559b4991ce78908c88eb4866cde2a3149b6e469
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-06-24 23:51:57 -0500

    added send email functionality

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ActionButton from '../components/ActionButton';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faClipboard } from '@fortawesome/free-regular-svg-icons';
+import{ init, sendForm } from 'emailjs-com';
+require('dotenv').config();
+init(process.env.REACT_APP_EMAILJS_UID);
 
 const Contact = (props) => {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  
+  const sendEmail = async (e) => {
+    let form = document.getElementById('contact_form');
+    e.preventDefault();
+    try {
+      let response = await sendForm('portolio_website', 'portfolio_message', form);
+      console.log(response);
+      console.log('...success?')
+    } catch (e) {
+      console.error(e);
+    } finally {
+      return 
+    }
+  }
 
   return (
     <div className="GenericContainer">
@@ -13,10 +32,12 @@ const Contact = (props) => {
           <p>christianvega.cvm@gmail.com</p>
           <FontAwesomeIcon icon={faClipboard}/>
         </div>
-        <input className="EmailContainer" placeholder="Your e-mail"></input>
-        <textarea className="MessageContainer" placeholder="Say hello 🙂" type="text"></textarea>
+        <form id='contact_form'>
+          <input className="EmailContainer" placeholder="your e-mail" value={email} onChange={(e)=>setEmail(e.target.value)} name='user_email'></input>
+          <textarea className="MessageContainer" placeholder="your message" type="text" value={message} onChange={(e)=>setMessage(e.target.value)} name='message'></textarea>
+        </form>
         <div className="ActionButtonCluster">
-          <ActionButton link="send-email" onChangeNav={props.onChangeNav}/>
+          <button onClick={sendEmail} className='EmailButton'>send e-mail</button>
         </div>
       </div>
     </div>
