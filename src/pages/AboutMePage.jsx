93ebde2b3747dc9c534ede39500a115753e0c9d2
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-07-08 01:14:16 -0500

    added instagram style css

--- src/pages/AboutMePage.jsx
+++ src/pages/AboutMePage.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import ActionButton from '../components/ActionButton';
 import profilePicture from '../assets/IMG_8239.JPG';
+import check from '../assets/checkmark.png';
 import LogosBannerNew from '../components/LogosBannerNew';
 import Footer from '../components/Footer';
 import IndustryExperience from '../components/IndustryExperience';
@@ -26,16 +27,22 @@ const AboutMe = (props) => {
       { isMounted ?
       <>
         <div className="ProfileContainer">
-          <img src={profilePicture} alt="Profile"></img>
+          <div className="ProfilePicture">
+            <div className="RainbowContainer">
+            </div>
+            <img src={profilePicture} alt="Profile"></img>
+          </div>
           <div className="ProfileTextContainer">
             <div>
-              <h1>Christian</h1>
-              <h1>Vega-Munguia</h1>
+              <h1>CHRISTIAN</h1>
+              <div>
+                <h1>VEGA-MUNGUIA<img src={check} alt=''/></h1>
+              </div>
             </div>
             <div>
-              <h2>🎓 UT Austin McCombs Grad</h2>
-              <h2>💼 Systems Analyst at Visa</h2>
-              <h2>🚀 Self-Taught Web Developer</h2>
+              <h2>🎓 Finance Grad from <a style={{color: '#ff6a00'}} href="http://www.utaustin.com">UT Austin</a></h2>
+              <h2>🖥️ Systems Analyst at <a style={{color: '#fcb70a'}} href="https://www.visa.com">Visa</a></h2>
+              <h2>🚀 Self-Taught<a style={{color: 'rgb(88, 214, 88)'}} href="http://www.utaustin.com"> Web Developer</a></h2>
             </div>
           </div>
         </div>
