1fac82938fcbcdf1577f163c8f69b5e28dcb5b3a
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-07-14 01:49:42 -0500

    added nav dropdown functionality for mobile

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ActionButton from './ActionButton';
 import NightModeButton from './NightModeButton';
 import NavBarLink from './NavBarLink';
@@ -7,10 +7,51 @@ import { faLinkedin, faMedium, faGithubSquare, faTwitterSquare } from '@fortawes
 
 const NavBar = (props) => {
   const [navIsExpanded, setNavIsExpanded] = useState(false);
+  const [dimensions, setDimensions] = useState({
+    height: window.innerHeight,
+    width: window.innerWidth
+  })
 
-  const toggleNav = (e) => {
+  useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        height: window.innerHeight,
+        width: window.innerWidth
+      })
+      // console.log({
+      //   height: window.innerHeight,
+      //   width: window.innerWidth
+      // });
+    }
+    window.innerWidth > 900 && !navIsExpanded ? setNavIsExpanded(true) : void(0)
+    window.innerWidth <= 900 && navIsExpanded ? setNavIsExpanded(false) : void(0)
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [dimensions])
+
+  useEffect(() => {
+    let linkButtons = document.getElementsByClassName('LinkButtonContainer');
+    // console.log(linkButtons.length);
+    // console.log(linkButtons);
+    // console.log(typeof(linkButtons));
+    // console.log('hi')
+    for (let i = 0; i < linkButtons.length; i++) {
+      console.log(linkButtons[i].style);
+      // console.log('hello')
+    }
+
+    return () => {
+    }
+  }, [navIsExpanded])
+
+
+
+  const toggleNav = () => {
     setNavIsExpanded(!navIsExpanded);
-    console.log('hi')
   }
 
   return (
@@ -25,28 +66,33 @@ const NavBar = (props) => {
             </div>
             <ActionButton link='contact' onChangeNav={props.onChangeNav}/>
           </div>
-          <div className="NavBarTop">
-            <ul className="LinkContainer">
-              <NavBarLink to='home' active={props.active}/>
-              <NavBarLink to='about-me' active={props.active}/>
-              <NavBarLink to='projects' active={props.active}/>
-              <NavBarLink to='articles' active={props.active}/>
-            </ul>
+          {
+            navIsExpanded &&
+          <div className="NavBarCollapsible">
+            <div className="NavBarTop">
+              <ul className="LinkContainer">
+                <NavBarLink to='home' active={props.active}/>
+                <NavBarLink to='about-me' active={props.active}/>
+                <NavBarLink to='projects' active={props.active}/>
+                <NavBarLink to='articles' active={props.active}/>
+              </ul>
+            </div>
+            <div className="NavBarBottom">
+              <NightModeButton/>
+              <div className="SocialIcons">
+                <a href='https://www.linkedin.com/in/christian-vm/'>
+                  <FontAwesomeIcon icon={faLinkedin} size='2x' />
+                </a>
+                <a href='https://medium.com/@christianvegaaa1'>
+                  <FontAwesomeIcon icon={faMedium} size='2x'/>
+                </a>
+                <a href='https://github.com/cvega21'>
+                  <FontAwesomeIcon icon={faGithubSquare} size='2x'/>
+                </a>
+              </div>
+            </div>
           </div>
-        </div>
-        <div className="NavBarBottom">
-          <NightModeButton/>
-          <div className="SocialIcons">
-            <a href='https://www.linkedin.com/in/christian-vm/'>
-              <FontAwesomeIcon icon={faLinkedin} size='2x' />
-            </a>
-            <a href='https://medium.com/@christianvegaaa1'>
-              <FontAwesomeIcon icon={faMedium} size='2x'/>
-            </a>
-            <a href='https://github.com/cvega21'>
-              <FontAwesomeIcon icon={faGithubSquare} size='2x'/>
-            </a>
-          </div>
+          }
         </div>
       </nav>
   )
