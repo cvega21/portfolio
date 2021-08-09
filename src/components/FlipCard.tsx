b369f7bfb753f6a1aed40fe38e9f1f0e0dea3d40
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-08-08 19:24:23 -0500

    added ts to industry experience, extracted flipcard component

---
+++ src/components/FlipCard.tsx
@@ -0,0 +1,28 @@
+import { FunctionComponent } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLandmark, faCommentsDollar, faBuilding, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+
+interface FlipCardProps {
+  icon: IconDefinition;
+  title: string;
+  description: string; 
+}
+
+const FlipCard: FunctionComponent<FlipCardProps> = ({icon, title, description}: FlipCardProps) => {
+  return (
+    <div className="flip-card">
+      <div className="flip-card-inner">
+        <div className="flip-card-front">
+          <FontAwesomeIcon icon={icon}/>
+          <h2>{title}</h2>
+        </div>
+        <div className="flip-card-back">
+          <h2>{description}</h2>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default FlipCard
---
+++ src/components/IndustryExperience.tsx
@@ -0,0 +1,20 @@
+import { FunctionComponent } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLandmark, faCommentsDollar, faBuilding } from '@fortawesome/free-solid-svg-icons';
+import FlipCard from './FlipCard';
+import '../styles/IndustrySection.scss'
+
+const IndustryExperience: FunctionComponent = () => {
+  return (
+    <div className="IndustrySection">
+      <h1>Industry Experience</h1>
+      <div className="IndustryGrid">
+        <FlipCard icon={faCommentsDollar} title={'Payments'} description={'Real-Time Fraud Prevention'}/>
+        <FlipCard icon={faLandmark} title={'Investment Banking'} description={'Consumer and Retail'}/>
+        <FlipCard icon={faBuilding} title={'Real Estate'} description={'Institutional Asset Management'}/>
+      </div>  
+    </div>
+    )
+}
+
+export default IndustryExperience
