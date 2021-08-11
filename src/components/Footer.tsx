dada6d5a39e3d1f3e49398134f160d159ffb900a
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-08-10 23:38:34 -0500

    fixed nightmode types

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
-import { FunctionComponent } from 'react';
+import React  from 'react';
 
-const Footer: FunctionComponent = () => {
+const Footer: React.FC = () => {
   return (
     <footer>
       <p>Made with <span>&#9829;</span> in Austin, TX</p>
--- src/components/NightModeButton.tsx
+++ src/components/NightModeButton.tsx
@@ -2,17 +2,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import React from 'react'
 
-const NightModeButton = () => {
-  const changeTheme = (e: any) => {
+const NightModeButton: React.FC = () => {
+  const changeTheme = (e: React.MouseEvent) => {
     document.body.classList.toggle("DarkTheme");
-    return
   }
   
   return (
     <div className="NightModeButtonContainer">
       <FontAwesomeIcon icon={faSun} size='lg' className="sunIcon"/>
       <label className="switch">
-        <input type="checkbox" onClick={changeTheme}/>
+        <input type="checkbox" onClick={e => changeTheme(e)}/>
         <span className="slider round"></span>
       </label>  
       <FontAwesomeIcon icon={faMoon} size='lg' className="moonIcon"/>
