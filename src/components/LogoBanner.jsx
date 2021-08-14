5e82a43e7c48e567b3e9bfb34635f07e3f52a7f6
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-08-13 21:10:58 -0500

    componentized logos banner segments

---
+++ src/components/LogoBanner.jsx
@@ -0,0 +1,58 @@
+import { useEffect } from 'react'
+import LogoSliderSegment from './LogoSliderSegment';
+
+const LogosBanner = () => {
+  useEffect(() => {
+    let topSliders = document.getElementsByClassName('logo-slider-segment');
+    let bottomSliders = document.getElementsByClassName('logo-slider-segment2');
+    let timer1;
+
+    if (topSliders.length && bottomSliders.length) {
+      try {
+        timer1 = setTimeout(() => {
+          topSliders[0].classList.toggle('scroll-fast');
+          topSliders[1].classList.toggle('scroll-fast');
+          bottomSliders[0].classList.toggle('scroll-med');
+          bottomSliders[1].classList.toggle('scroll-med');
+        }, 750)
+      } catch (e) {
+        console.error(e);
+      }
+    }
+    
+    return () => {
+      clearTimeout(timer1);
+    }
+  }, [])
+
+  return (
+    <div className="logos-banner">
+      <h1>Tech Experience</h1>
+      <h2>work + personal projects</h2>
+      <div className="logo-slider-container">
+          <div className="logo-slider">  
+            <div className="fade-background fade-background-left"></div>
+            <div className="logo-slider-segment">
+              <LogoSliderSegment logos={['html', 'css', 'javascript', 'react', 'mongo', 'firebase', 'mysql', 'python']}/>
+            </div>
+            <div className="logo-slider-segment">
+              <LogoSliderSegment logos={['html', 'css', 'javascript', 'react', 'mongo', 'firebase', 'mysql', 'python']}/>
+            </div>
+            <div className="fade-background fade-background-right"></div>
+          </div>
+          <div className="logo-slider">  
+            <div className="fade-background fade-background-left"></div>
+            <div className="logo-slider-segment2">
+            <LogoSliderSegment logos={['heroku', 'jenkins', 'jira', 'rhel', 'tomcat', 'bash']}/>
+            </div>
+            <div className="logo-slider-segment2">
+              <LogoSliderSegment logos={['heroku', 'jenkins', 'jira', 'rhel', 'tomcat', 'bash']}/>
+            </div>
+            <div className="fade-background fade-background-right"></div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default LogosBanner
--- src/components/LogoSliderSegment.tsx
+++ src/components/LogoSliderSegment.tsx
@@ -1,17 +1,18 @@
 import { logosDict } from './LogoSliderDict'
+import React, { useState } from 'react'
 
 interface LogoSliderProps {
   logos: string[]
 }
 
 const LogoSliderSegment = ({ logos }: LogoSliderProps) => {
+  const images = logos.map((elem: string) => 
+      <img key={logosDict[elem].src} src={logosDict[elem].src} alt={logosDict[elem].alt} className={(logosDict[elem].invert === true ? 'invert' : 'noInvert')}/>
+    )
+  
   return (
     <>
-      {    
-        logos.map((elem: string) => {
-          return <img src={logosDict[elem].src} alt={logosDict[elem].alt} className={(logosDict[elem].invert === true ? 'invert' : 'noInvert')}/>
-        })
-      }
+      {images}
     </>
   )
 }
