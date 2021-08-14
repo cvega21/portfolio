59b4085ad7f498728cd070c61172ac51005c891d
Christian Vega-Munguia <christianvega.cvm@gmail.com>
2021-08-13 23:47:40 -0500

    added ts to resume page

--- src/pages/ResumePage.tsx
+++ src/pages/ResumePage.tsx
@@ -3,13 +3,11 @@ import ActionButton from '../components/ActionButton';
 
 const ResumePage = () => {
   return (
-    // <div className="GenericContainer">
       <div className="ResumeContainer">
         <h1>coming soon...</h1>
         <h2>✍🏽</h2>
       <ActionButton link='aboutme' navigation='left'/>
       </div>
-    // </div>
   )
 }
 
